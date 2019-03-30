9b2a8718da6a0c9d97b03c9bd8ce9ee091316f17
ramirezremy-21 <remy.ramirez06@gmail.com>
2019-03-30 17:43:07 +0100

    exos 1 à 5

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" #Calcul du nombres d'heures durant le bootcamp THP à raison sur 10 semaines 5 jours par semaine à raison de 11h de taf
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #conversion en minutes
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 #conversion en Secondes
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7 #La condition étant fausse le résultat est false
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #Calcul de la condition 1
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #Calcul de la condition 2
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #Test sur les sup ou égal
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #Test sur les sup ou égal
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #Test sur les sup ou égal
